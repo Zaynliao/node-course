836b87bdf80fa1ce4a310df63320a330ae5f8ec2
Zaynliao <liaozayn@gmail.com>
2022-05-08 14:30:13 +0800

    sum.js

---
+++ basic/sum.js
@@ -0,0 +1,14 @@
+function sum(n){
+    let i=0;
+    let sum=0;
+    for(i=0;i<=n;i++){
+    sum+=i;
+    }
+return sum;
+}
+
+
+console.log(sum(1));
+console.log(sum(10));
+console.log(sum(50));
+console.log(sum(100));
