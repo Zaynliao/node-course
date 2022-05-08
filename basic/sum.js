5382ff41dc5cd7db08695038b8453910661af5a2
Zaynliao <liaozayn@gmail.com>
2022-05-08 14:34:56 +0800

    sum3.js

--- basic/sum.js
+++ basic/sum.js
@@ -11,6 +11,8 @@ function sum2(n){
     return (n+1)*n/2;
 }
 
+let sum3 = (n)=>(n+1)*n/2;
+
 
 console.log(sum(1));
 console.log(sum(10));
@@ -20,3 +22,7 @@ console.log(sum2(1));
 console.log(sum2(10));
 console.log(sum2(50));
 console.log(sum2(100));
+console.log(sum3(1));
+console.log(sum3(10));
+console.log(sum3(50));
+console.log(sum3(100));
