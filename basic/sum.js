d06d44975d5925fcf9abd7508768c900c8c3fd95
Zaynliao <liaozayn@gmail.com>
2022-05-08 15:32:39 +0800

    新增 reduce 寫法

--- basic/sum.js
+++ basic/sum.js
@@ -14,15 +14,40 @@ function sum2(n) {
 let sum3 = (n) => (n + 1) * n / 2;
 
 
-console.log(sum(1));
-console.log(sum(10));
-console.log(sum(50));
-console.log(sum(100));
-console.log(sum2(1));
-console.log(sum2(10));
-console.log(sum2(50));
-console.log(sum2(100));
-console.log(sum3(1));
-console.log(sum3(10));
-console.log(sum3(50));
-console.log(sum3(100));
+
+function sum4(n) {
+    let Arr = [];
+    let k = 0;
+    for (k; k <= n; k++) {
+        Arr.push(k);
+    }
+
+    let sum = Arr.reduce((total, n) => total + n,
+        0);
+    return sum;
+}
+
+
+
+
+console.log("-------------------");
+console.log("sum ->" + sum(1));
+console.log("sum ->" + sum(10));
+console.log("sum ->" + sum(50));
+console.log("sum ->" + sum(100));
+console.log("-------------------");
+console.log("sum2 ->" + sum2(1));
+console.log("sum2 ->" + sum2(10));
+console.log("sum2 ->" + sum2(50));
+console.log("sum2 ->" + sum2(100));
+console.log("-------------------");
+console.log("sum3 ->" + sum3(1));
+console.log("sum3 ->" + sum3(10));
+console.log("sum3 ->" + sum3(50));
+console.log("sum3 ->" + sum3(100));
+console.log("-------------------");
+console.log("sum4 ->" + sum4(1));
+console.log("sum4 ->" + sum4(10));
+console.log("sum4 ->" + sum4(50));
+console.log("sum4 ->" + sum4(100));
+console.log("-------------------");
