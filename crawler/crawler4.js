9a0fb3a705fe6f974844db0a4de8026610707f5f
Zaynliao <liaozayn@gmail.com>
2022-05-21 14:14:38 +0800

    新增env

--- crawler/crawler4.js
+++ crawler/crawler4.js
@@ -4,11 +4,11 @@ require("dotenv").config();
 (async() => {
 
     const connection = await mysql.createConnection({
-        host: 'localhost',
-        port: 3306,
-        user: 'admin',
-        password: process.env.OPENWEATER_TOKEN,
-        database: 'stock'
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
     })
 
     let [data, filed] = await connection.execute('SELECT * FROM stocks');
